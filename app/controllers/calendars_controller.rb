2a17e0a52d435bbea5c9f48e8e90e522de9dc6ec
Te-Ma-LW <5070maco@gmail.com>
2022-06-16 12:04:42 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -5,7 +5,6 @@ class CalendarsController < ApplicationController
     get_week
     @plan = Plan.new
   end
-
   # 予定の保存
   def create
     Plan.create(plan_params)
@@ -20,7 +19,7 @@ class CalendarsController < ApplicationController
 
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
+    # puts wdays[毎回変わる1]
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
@@ -34,9 +33,21 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+
+      wday_num = @todays_date.wday + x # wdayメソッドを用いて取得した数値
+      if wday_num > 6 #「wday_numが7以上の場合」という条件式
+        wday_num = wday_num -7
+      end
+
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
+
       @week_days.push(days)
     end
-
   end
 end
+
+#やること→曜日の表示
+# wdays[数字]
+# num = 0 , wdays[num] → 日曜日
+
+#やること→ 曜日を次のものにする
